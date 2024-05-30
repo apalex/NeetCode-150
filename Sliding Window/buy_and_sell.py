a435d67798ccc7b591df1183acb8434bd65d6278
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-05-30 09:10:04 -0400

    Create buy_and_sell.py

---
+++ Sliding Window/buy_and_sell.py
@@ -0,0 +1,41 @@
+#Buy and Sell Crypto
+
+#https://www.youtube.com/watch?v=1pkOgXD63yU&ab_channel=NeetCode
+
+#You are given an integer array prices where prices[i] is the price of NeetCoin on the ith day.
+#You may choose a single day to buy one NeetCoin and choose a different day in the future to sell it.
+
+#Return the maximum profit you can achieve. You may choose to not make any transactions, in which case the profit would be 0.
+
+#Example 1:
+
+#Input: prices = [10,1,5,6,7,1]
+#Output: 6
+
+#Explanation: Buy prices[1] and sell prices[4], profit = 7 - 1 = 6.
+
+#Example 2:
+
+#Input: prices = [10,8,7,5,2]
+#Output: 0
+
+#Explanation: No profitable transactions can be made, thus the max profit is 0.
+
+#Constraints:
+#1 <= prices.length <= 100
+#0 <= prices[i] <= 100
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy, sell = 0, 1
+        maxP = 0
+
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                profit = prices[sell] - prices[buy]
+                maxP = max(maxP, profit)
+            else:
+                buy = sell
+            sell += 1
+        
+        return maxP
