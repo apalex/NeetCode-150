5fc2a6824d055143afa9342038f5ec407925d186
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-06-10 19:12:59 -0400

    Create longest_consecutive_sequence.py

---
+++ Arrays & Hashing/longest_consecutive_sequence.py
@@ -0,0 +1,40 @@
+#Longest Consecutive Sequence
+
+#Medium
+#https://www.youtube.com/watch?v=P6RZZMu_maU&ab_channel=NeetCode
+
+#Given an array of integers nums, return the length of the longest consecutive
+#sequence of elements. A consecutive sequence is a sequence of elements in which
+#each element is exactly 1 greater than the previous element.
+#You must write an algorithm that runs in O(n) time.
+
+#Example 1:
+
+#Input: nums = [2,20,4,10,3,4,5]
+#Output: 4
+
+#Explanation: The longest consecutive sequence is [2, 3, 4, 5].
+
+#Example 2:
+
+#Input: nums = [0,3,2,5,4,6,1,1]
+#Output: 7
+
+#Constraints:
+#0 <= nums.length <= 1000
+#-10^9 <= nums[i] <= 10^9
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        longest = 0
+
+        #O(nlogn)
+        for n in numSet:
+            if (n - 1) not in numSet:
+                length = 1
+                while (n + length) in numSet:
+                    length += 1
+                longest = max(longest, length)
+        return longest
+
