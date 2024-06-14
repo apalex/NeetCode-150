49087d7e3845f730e3ed5806a6f7b478f650ad95
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-06-14 11:36:11 -0400

    Update longest_consecutive_sequence.py

--- Arrays & Hashing/longest_consecutive_sequence.py
+++ Arrays & Hashing/longest_consecutive_sequence.py
@@ -29,7 +29,7 @@ class Solution:
         numSet = set(nums)
         longest = 0
 
-        #O(nlogn)
+        #O(n)
         for n in numSet:
             if (n - 1) not in numSet:
                 length = 1
