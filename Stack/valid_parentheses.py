c2f60507e75618d23eec9d4e9589161755f9e490
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-06-19 17:29:46 -0400

    Create valid_parentheses.py

---
+++ Stack/valid_parentheses.py
@@ -0,0 +1,46 @@
+#Validate Parentheses
+
+#Easy
+#https://www.youtube.com/watch?v=WTzjTskDFMg&ab_channel=NeetCode
+
+#You are given a string s consisting of the following characters: '(', ')', '{', '}', '[' and ']'.
+#The input string s is valid if and only if:
+#Every open bracket is closed by the same type of close bracket.
+#Open brackets are closed in the correct order.
+#Every close bracket has a corresponding open bracket of the same type.
+#Return true if s is a valid string, and false otherwise.
+
+#Example 1:
+
+#Input: s = "[]"
+#Output: true
+
+#Example 2:
+
+#Input: s = "([{}])"
+#Output: true
+
+#Example 3:
+
+#Input: s = "[(])"
+#Output: false
+
+#Explanation: The brackets are not closed in the correct order.
+
+#Constraints:
+#1 <= s.length <= 1000
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False
+            stack.pop()
+
+        return not stack
