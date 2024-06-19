d5ef4a466cd06c248eee1677d162ac4aac2a8bf5
Alex Pham <67773092+apalex@users.noreply.github.com>
2024-06-19 17:43:09 -0400

    Update valid_parentheses.py

--- Stack/valid_parentheses.py
+++ Stack/valid_parentheses.py
@@ -36,6 +36,23 @@ class Solution:
         stack = []
 
         for c in s:
+            if c in Map:
+                if stack and stack[-1] == Map[c]:
+                    stack.pop()
+                else:
+                    return False
+            else:
+                stack.append(c)
+        return not stack 
+     
+#OR
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        Map = {")": "(", "]": "[", "}": "{"}
+        stack = []
+
+        for c in s:
             if c not in Map:
                 stack.append(c)
                 continue
